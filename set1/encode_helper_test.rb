cb19c6e68632f676d241847433cacd9d592840ce
Christian Poplawski <christianpoplawski@gmail.com>
2019-02-12 21:09:36 +0100

    More detailed unit tests

--- set1/encode_helper_test.rb
+++ set1/encode_helper_test.rb
@@ -2,10 +2,24 @@ require 'test/unit'
 require_relative 'encode_helper'
 
 class Base64Test < Test::Unit::TestCase
-  def test_encoder
+  def test_hex_to_base64
     assert_equal(
       'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t',
       EncodeHelper.hex_to_base64('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d')
+    )
+  end
+
+  def test_decode_hex_string
+    assert_equal(
+      'I\'m killing your brain like a poisonous mushroom',
+      EncodeHelper.decode_hex_string('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d')
+    )
+  end
+
+  def test_encode_to_base64
+    assert_equal(
+      'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t',
+      EncodeHelper.encode_to_base64('I\'m killing your brain like a poisonous mushroom')
     )
   end
 end
